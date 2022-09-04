c8ad987549d1326fe988733e949e0ba835f23f1e
kiipod <iamkir90@hotmail.com>
2022-09-04 14:17:35 +0300

    Правки по замечаниям

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE taskforce CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE taskforce;
 
-// Таблица городов
+/* Таблица городов */
 CREATE TABLE cities (
   id int AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -9,14 +9,14 @@ CREATE TABLE cities (
   longtitude DECIMAL(11, 8) NOT NULL
 );
 
-// Таблица категорий заданий
+/* Таблица категорий заданий */
 CREATE TABLE categories (
   id int AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   icon VARCHAR (20)
 );
 
-// Таблица пользователей
+/* Таблица пользователей */
 CREATE TABLE users (
   id int AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -24,23 +24,30 @@ CREATE TABLE users (
   password VARCHAR(64) NOT NULL,
   city_id int NOT NULL,
   date_creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  category_id int NOT NULL,
-  rating int DEFAULT 0,
+  rating TINYINT DEFAULT 0,
   popularity int DEFAULT 0,
-  avatar_file int NULL,
+  avatar_file_id int NULL,
   birthday DATETIME,
   phone VARCHAR(32),
   telegram VARCHAR(64),
   bio TEXT,
   orders_num int DEFAULT 0,
-  head_card_status TINYINT UNSIGNED NOT NULL,
+  status TINYINT UNSIGNED NOT NULL,
   is_executor BOOL NOT NULL,
   FOREIGN KEY (category_id) REFERENCES categories (id),
   FOREIGN KEY (avatar_file) REFERENCES files (id),
   FOREIGN KEY  (city_id) REFERENCES cities (id)
 );
 
-// Таблица задач
+/* Таблца связывающая исполнителя и категории */
+CREATE TABLE executor_category (
+  category_id int NOT NULL,
+  user_id int NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+/* Таблица задач */
 CREATE TABLE tasks (
   id int AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -49,7 +56,7 @@ CREATE TABLE tasks (
   date_creation DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   category_id int NOT NULL,
   customer_id int NOT NULL,
-  executor_id int NOT NULL,
+  executor_id int,
   status TINYINT NOT NULL,
   budget int NOT NULL,
   period_execution DATETIME NOT NULL,
@@ -59,13 +66,13 @@ CREATE TABLE tasks (
   FOREIGN KEY (city_id) REFERENCES cities (id)
 );
 
-// Таблица файлов
+/* Таблица файлов */
 CREATE TABLE files (
   id int AUTO_INCREMENT PRIMARY KEY,
   url VARCHAR(255) NOT NULL UNIQUE
 );
 
-// Таблица файлов задач
+/* Таблица файлов задач */
 CREATE TABLE tasks_files (
   id int AUTO_INCREMENT PRIMARY KEY,
   task_id int NOT NULL,
@@ -74,7 +81,7 @@ CREATE TABLE tasks_files (
   FOREIGN KEY (file_id) REFERENCES files (id)
 );
 
-// Таблица отзывов о исполнителях
+/* Таблица отзывов о исполнителях */
 CREATE TABLE feedback (
   id int AUTO_INCREMENT PRIMARY KEY,
   customer_id int NOT NULL,
@@ -88,7 +95,7 @@ CREATE TABLE feedback (
   FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
 
-// Таблица с откликами исполнителей
+/* Таблица с откликами исполнителей */
 CREATE TABLE response (
   id int AUTO_INCREMENT PRIMARY KEY,
   date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -96,6 +103,7 @@ CREATE TABLE response (
   executor_id int NOT NULL,
   price int,
   comment TEXT,
+  refuse TINYINT,
   FOREIGN KEY (executor_id) REFERENCES users (id),
   FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
